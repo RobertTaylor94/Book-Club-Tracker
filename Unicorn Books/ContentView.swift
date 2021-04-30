9d78688a77fce80d6e6d17f725bc53fb9545f0a7
Robert Taylor <rjt903@gmail.com>
2021-04-30 13:55:34 +0100

    Initial Commit

---
+++ Unicorn Books/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Unicorn Books
+//
+//  Created by Robert Taylor on 30/04/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Unicorn Books/Unicorn_BooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Unicorn_BooksApp.swift
+//  Unicorn Books
+//
+//  Created by Robert Taylor on 30/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct Unicorn_BooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
