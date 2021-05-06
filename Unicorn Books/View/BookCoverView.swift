69df1e90bbdbb0e19c27c4c9190d3a6d41a6a351
RobertTaylor94 <rjt903@gmail.com>
2021-05-06 17:25:33 +0100

    Added modal sheet to the CoverView to launch into details from the home screen

--- Unicorn Books/View/BookCoverView.swift
+++ Unicorn Books/View/BookCoverView.swift
@@ -13,46 +13,61 @@ struct BookCoverView: View {
     
     let realm = try! Realm()
     
-    @State var progressValue: Float = 0.0
+    @State var progressValue: Float = 0.01
     @State var bookID: String = ""
     @State var bookTitle: String = ""
     @State var bookAuthor: String = ""
     @State var bookDescription: String = ""
     @State var bookImgUrl: String = ""
+    @State var book: DBBook
+    @State private var isShowingSheet = false
     
     
     var body: some View {
-        
-        VStack(spacing: 25) {
-            
-            VStack(alignment: .leading, spacing: 16.0) {
-                if bookImgUrl != "" {
-                    WebImage(url: URL(string: bookImgUrl)).resizable().frame(width: 120, height: 170)
-                } else {
-                    Image(systemName: "books.vertical").resizable().frame(width: 120, height: 170)
+    
+            VStack(spacing: 20) {
+                
+                Button(action: {
+                    isShowingSheet.toggle()
+                }, label: {
+                    VStack(alignment: .leading, spacing: 16.0) {
+                        if bookImgUrl != "" {
+                            WebImage(url: URL(string: bookImgUrl)).resizable().frame(width: 120, height: 170)
+                        } else {
+                            Image(systemName: "books.vertical").resizable().frame(width: 120, height: 170)
+                        }
+                        Text("\(bookTitle)")
+                            .font(.title)
+                            .fontWeight(.bold)
+                        Text("\(bookAuthor)")
+                            .font(.caption)
+                        Text("\(bookDescription)")
+                            .fontWeight(.light)
+                            .lineLimit(4)
+                    }
+                    .frame(width: 200, height: 350)
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(LinearGradient(gradient: Gradient(colors: [Color("BookTileColorOne"), Color("BookTileColorTwo")]), startPoint: .topLeading, endPoint: .bottomTrailing))
+                    .cornerRadius(20)
+                })
+                .sheet(isPresented: $isShowingSheet) {
+                    DetailView(progressValue: progressValue, bookID: bookID, bookTitle: bookTitle, bookAuthor: bookAuthor, bookDescription: bookDescription, bookImgUrl: bookImgUrl)
                 }
-                Text("\(bookTitle)")
-                    .font(.title)
-                    .fontWeight(.bold)
-                Text("\(bookAuthor)")
-                    .font(.caption)
-    //            Image()
-                Text("\(bookDescription)")
-                    .fontWeight(.light)
-                    .lineLimit(4)
-            }
-            .frame(width: 200, height: 350)
-            .foregroundColor(.white)
-            .padding()
-            .background(LinearGradient(gradient: Gradient(colors: [Color.red, Color.blue]), startPoint: .topLeading, endPoint: .bottomTrailing))
-            .cornerRadius(20)
-            
-            ProgressBar(progress: self.$progressValue)
-                .frame(width: 100, height: 100)
-                .padding()
+                
+        
+               
+                
+                ProgressBar(progress: self.$progressValue)
+                    .frame(width: 100, height: 100)
+                    .padding()
 
-            Stepper("", value: $progressValue, in: 0...1, step: 0.1)
+                Stepper("", value: $progressValue, in: 0...1, step: 0.1)
+                    .onChange(of: progressValue, perform: { value in
+                        saveBook(book: book)
+                    })
 
+                
             
         }
     }
@@ -72,7 +87,7 @@ struct BookCoverView: View {
 
 struct BookCover_Previews: PreviewProvider {
     static var previews: some View {
-        BookCoverView()
+        BookCoverView(book: DBBook())
     }
 }
 
