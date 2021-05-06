876d4ecfbab504968278393e52b841041de79bba
RobertTaylor94 <rjt903@gmail.com>
2021-05-06 17:26:03 +0100

    Created DetailView to show expanded book details from the home screen

--- Unicorn Books/View/DetailView.swift
+++ Unicorn Books/View/DetailView.swift
@@ -6,10 +6,54 @@
 //
 
 import SwiftUI
+import SDWebImageSwiftUI
+import RealmSwift
 
 struct DetailView: View {
+    
+    @State var progressValue: Float = 0.4
+    @State var bookID: String = ""
+    @State var bookTitle: String = ""
+    @State var bookAuthor: String = ""
+    @State var bookDescription: String = ""
+    @State var bookImgUrl: String = ""
+    @Environment(\.presentationMode) var presentationMode
+    @State private var books: Results<DBBook>?
+    let realm = try! Realm()
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack(spacing: 20) {
+            Text(bookTitle).font(.title2)
+            Text(bookAuthor).font(.caption)
+            WebImage(url: URL(string: bookImgUrl)).resizable().frame(width: 200, height: 300).cornerRadius(10)
+            ScrollView {
+                Text(bookDescription).font(.caption)
+            }.padding()
+            ProgressBar(progress: $progressValue)
+        }
+        .padding()
+        .background(LinearGradient(gradient: Gradient(colors: [Color("DetailViewColorOne"), Color("DetailViewColorTwo")]), startPoint: .topLeading, endPoint: .bottomTrailing).ignoresSafeArea())
+        
+        
+        
+        
+    }
+    
+    func removeBook() {
+        
+        books = realm.objects(DBBook.self)
+        books = books?.filter("title CONTAINS %@", bookTitle)
+        
+        do {
+            try realm.write {
+                realm.delete(books!)
+            }
+        } catch {
+            print("Error deleting from context, \(error)")
+        }
+        
     }
 }
 
