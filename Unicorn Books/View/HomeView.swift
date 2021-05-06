457191120785e5a71ef677158a6c0bbb653e72d2
RobertTaylor94 <rjt903@gmail.com>
2021-05-06 17:26:34 +0100

    Added Delete option to books in user library

--- Unicorn Books/View/HomeView.swift
+++ Unicorn Books/View/HomeView.swift
@@ -10,30 +10,44 @@ import RealmSwift
 
 struct HomeView: View {
     
-    @AppStorage("onBoardingShowing") var isOnboardingShowing = true
+    //    @AppStorage("onBoardingShowing") var isOnboardingShowing = true
     @State private var books: Results<DBBook>?
     let realm = try! Realm()
     
     var body: some View {
+        
         NavigationView {
             VStack {
-//                Text("Unicorn Book Club")
                 Spacer()
                 
                 if books != nil {
-                
-                ScrollView(.horizontal, showsIndicators: false) {
-                    HStack {
-                        ForEach(books!) { i in
-                            BookCoverView(progressValue: i.progressValue, bookID: i.id, bookTitle: i.title, bookAuthor: i.authors, bookDescription: i.desc, bookImgUrl: i.imgurl)
+                    
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack {
+                            ForEach(books!.reversed()) { i in
+                                ZStack {
+                                    BookCoverView(progressValue: i.progressValue, bookID: i.id, bookTitle: i.title, bookAuthor: i.authors, bookDescription: i.desc, bookImgUrl: i.imgurl, book: i)
+                                    VStack(alignment: .trailing) {
+                                        HStack {
+                                            Spacer()
+                                            Button(action: {
+                                            deleteBook(filter: i.title)
+                                            updateBooks()
+                                        }, label: {
+                                            Text("Delete")
+                                                .font(.caption).foregroundColor(.white).padding().background(Color.red).cornerRadius(20).shadow(color: .black, radius: 10, x: 0.0, y: 0.0)
+                                    })
+                                        }.padding()
+                                        Spacer()
+                                    }
+                                }
+                            }
                         }
                     }
-                }
-                .onAppear(){
-                    updateBooks()
-                }
-                .padding()
-                    
+                    .onAppear(){
+                        updateBooks()
+                    }
+                    .padding()
                 } else {
                     Text("Add Your First Book!")
                         .padding()
@@ -41,29 +55,32 @@ struct HomeView: View {
                             updateBooks()
                         }
                 }
-                
-                
-                
-                NavigationLink("Add Book", destination: AddBookView())
-                    .foregroundColor(.white)
-                    .font(.body)
-                    .padding()
-                    .background(Color("mainTextColor"))
-                    .cornerRadius(20)
                 Spacer()
+                    
+                    .navigationTitle("Book Club")
             }
-            .navigationTitle("Book Club")
-//            .navigationBarHidden(true)
+            
         }
-        
-        
     }
     
     func updateBooks() {
         books = realm.objects(DBBook.self)
     }
+    func deleteBook(filter: String) {
+        let book = books?.filter("title CONTAINS %@", filter)
+        
+        do {
+            try realm.write {
+                realm.delete(book!)
+            }
+        } catch {
+            print("Error deleting from context, \(error)")
+        }
+    }
     
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
